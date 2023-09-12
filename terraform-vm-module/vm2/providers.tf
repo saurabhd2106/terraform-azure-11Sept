037f824a197c0cef000c4d5b9f4ee2d913236c3a
Saurabh Dhingra <saurabh.d2106@gmail.com>
2023-09-12 17:19:04 +0530

    Update providers.tf

--- terraform-vm-module/vm2/providers.tf
+++ terraform-vm-module/vm2/providers.tf
@@ -17,10 +17,5 @@ provider "azurerm" {
 
   }
 
-  client_id     = "eace8cba-1ab4-4b4f-a6ff-b084b288df1b"
-  client_secret = "Zb98Q~790uXp6hc-HcQP1gRMCOtUdZRnvgQumat7"
-
-  tenant_id       = "fa1e26bd-a406-4570-a09c-e0f3083223c6"
-  subscription_id = "e0b47bea-67c1-44d3-a3ae-a494d59ab222"
-
+ 
 }
