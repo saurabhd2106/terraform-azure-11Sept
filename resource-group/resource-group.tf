c8aa4cfbc41b5d495586959aabb636c64e6fe161
saurabh <saurabh.d2106@gmail.com>
2023-09-12 11:15:35 +0100

    module

--- resource-group/resource-group.tf
+++ resource-group/resource-group.tf
@@ -2,8 +2,16 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg-name
   location = var.location
 
-  tags = {
-    "env" : var.envName
-  }
+  tags = local.custom_tags
 
+}
+
+locals {
+  
+  custom_tags = {
+
+    "env" : var.envName
+    "account" : "my-org"
+
+  }
 }
